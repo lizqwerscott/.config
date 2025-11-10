2ace9347fd72b0b804cc1b868342cd5b2fe8f81f
lizqwerscott <lizqwerscott@163.com>
2025-11-10 19:29:13 +0800

    feat: add background static and waly for hyprland

---
+++ hypr/scripts/background-static.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+WALLPAPER_DIR="$HOME/Pictures/background/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+sleep 10
+
+while true; do
+    # Get a random wallpaper that is not the current one
+    WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+    # Apply the selected wallpaper
+    hyprctl hyprpaper reload ,"$WALLPAPER"
+    sleep 60
+done
---
+++ hypr/scripts/background-video.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sleep 10
+wallpaperplay mute /home/lizqwer/Videos/wallpapers
